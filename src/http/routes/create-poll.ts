aebd3816b08e88a7862210dedf557ae7b5cfb7ff
Laura de Oliveira <l4ur4.88@gmail.com>
2024-02-06 19:33:10 +0000

    Refactor server connections and routes

---
+++ src/http/routes/create-poll.ts
@@ -0,0 +1,23 @@
+import z from "zod"
+import { FastifyInstance } from "fastify"
+import { prisma } from "../../lib/prisma"
+
+export async function createPoll(app: FastifyInstance) {
+  app.post("/polls", async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+      data: {
+        title
+      }
+    })
+
+    return reply.status(201).send({
+      pollId: poll.id
+    })
+  })
+}
