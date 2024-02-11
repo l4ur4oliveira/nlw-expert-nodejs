108adb8d702fbba38d95be63476a44257ccaab87
Laura de Oliveira <l4ur4.88@gmail.com>
2024-02-11 00:48:53 +0000

    Add Pub/Sub pattern to send vote count via websocket

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import z from "zod"
 import { FastifyInstance } from "fastify"
 import { prisma } from "../../lib/prisma"
 import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -40,7 +41,12 @@ export async function voteOnPoll(app: FastifyInstance) {
           }
         })
 
-        await redis.zincrby(pollId, -1, userPrevVote.pollOptionId)
+        const votes = await redis.zincrby(pollId, -1, userPrevVote.pollOptionId)
+
+        voting.publish(pollId, {
+          pollOptionId: userPrevVote.pollOptionId,
+          votes: Number(votes)
+        })
       }
     }
 
@@ -63,7 +69,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     })
 
-    await redis.zincrby(pollId, 1, pollOptionId)
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
+    })
 
     return reply.status(201).send()
   })
